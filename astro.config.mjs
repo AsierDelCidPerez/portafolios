fa1c101a00c22e8f0fa9583c8c128a95243bd9e7
Asier Del Cid Pérez <103944663+AsierDelCidPerez@users.noreply.github.com>
2025-02-08 17:23:07 +0100

    despliegue 1.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://asierdelcidperez.github.io",
-  base: "portafolios",
+  base: "/portafolios",
   server: {
     port: 3000
   },
