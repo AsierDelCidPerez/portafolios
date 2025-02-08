ccf755b948d1da88277b00436d472ebdbb2ea9b0
Asier Del Cid Pérez <103944663+AsierDelCidPerez@users.noreply.github.com>
2025-02-08 16:36:16 +0100

    Mi portfolio

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,6 +1,6 @@
-export const socials = [{ logo: 'bx bxl-youtube' }, { logo: 'bx bxl-linkedin-square' }, { logo: 'bx bxl-instagram' }, { logo: 'bx bxl-facebook-circle' }, { logo: 'bx bxl-github' }];
+export const socials = [ {logo: 'bx bxl-linkedin-square', link: 'https://es.linkedin.com/in/asier-del-cid-p%C3%A9rez-0198b2233'}, { logo: 'bx bxl-instagram', link: 'https://www.instagram.com/adelcidp142/' }, { logo: 'bx bxl-github', link: 'https://github.com/AsierDelCidPerez' }, {logo: 'bx bxl-twitter', link: 'https://twitter.com/adelcidp_175'} ];
 
-export const info = [{ text: 'About' }, { text: 'Projects' }];
+export const info = [{ text: 'Sobre mi' }, { text: 'Proyectos' }];
 
 export const technologies = [
    {
@@ -12,12 +12,12 @@ export const technologies = [
       logo: 'bx bxl-javascript',
    },
    {
-      text: 'PostgreSQL',
-      logo: 'bx bxl-postgresql',
+      text: 'MongoDB Atlas',
+      logo: 'bx bxl-mongodb',
    },
    {
-      text: 'AWS',
-      logo: 'bx bxl-aws',
+      text: 'SQL',
+      logo: 'bx bxl-postgresql',
    },
    {
       text: 'Docker',
@@ -43,65 +43,20 @@ export const technologies = [
       text: 'GitHub',
       logo: 'bx bxl-github',
    },
+   {
+      text: 'Azure',
+      logo: 'bx bx-pyramid',
+   }
 ];
 
 export const projects = [
    {
-      title: 'Blog',
+      title: 'XChanger',
       description:
-         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
-      image: 'image-blog',
+         'Un programa creado para que estudiantes de la universidad puedan intercambiarse los grupos.',
+      image: 'xchanger',
       color: '#f3c201',
-   },
-   {
-      title: 'Chatgpt API',
-      description:
-         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. .',
-      image: 'image-chatgpt-api',
-      color: '#1629ab',
-   },
-   {
-      title: 'Ecommerce',
-      description:
-         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
-      image: 'image-ecommerce',
-      color: '#045fb7',
-   },
-   {
-      title: 'Facebook Clone',
-      description:
-         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
-      image: 'image-facebook-clone',
-      color: '#146866',
-   },
-   {
-      title: 'Instagram Clone',
-      description:
-         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
-      image: 'image-instagram-clone',
-      color: '#17193f',
-   },
-   {
-      title: 'Slider',
-      description:
-         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
-      image: 'image-slider',
-      color: '#bbbbbb',
-   },
-   {
-      title: 'Spotify Clone',
-      description:
-         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
-      image: 'image-spotify-clone',
-      color: '#49d53c',
-   },
-   {
-      title: 'Youtube Clone',
-      description:
-         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
-      image: 'image-youtube-clone',
-      color: '#f7030c',
-   },
+   }
 ];
 
 export const buttons = [
@@ -109,6 +64,7 @@ export const buttons = [
       github: 'bx bxl-github',
       demo: 'bx bx-desktop',
       back: 'bx bxs-left-arrow-circle',
-      hrefgithub: 'https://github.com/MilaDev-JS'
+      hrefgithub: 'https://github.com/AsierDelCidPerez/xChanger',
+      livedemo: 'https://cambiargrupouc.onrender.com/'
    },
 ];
